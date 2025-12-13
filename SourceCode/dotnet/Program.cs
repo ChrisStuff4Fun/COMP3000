3d62c4bda2d72b627bae49f9aeabb6863ddbc1f7
Chris Catlin <109528250+ChrisStuff4Fun@users.noreply.github.com>
2025-12-13 13:35:30 +0000

    More logging code
    Settings to add further details to logs

--- SourceCode/dotnet/Program.cs
+++ SourceCode/dotnet/Program.cs
@@ -14,6 +14,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
 
 
 
