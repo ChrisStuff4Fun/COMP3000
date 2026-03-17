ed7f7defd1b57460411ed72312425c8c1fc702a7
Chris Catlin <109528250+ChrisStuff4Fun@users.noreply.github.com>
2026-03-17 14:51:06 +0000

    Bug Fix: Added () to bound check

--- SourceCode/React/react-frontend/src/pages/Dashboard.js
+++ SourceCode/React/react-frontend/src/pages/Dashboard.js
@@ -88,9 +88,9 @@ function pointInPolygon(point, polygon) {
     const [xi, yi] = polygon[i];
     const [xj, yj] = polygon[j];
 
-    const intersect =
-      yi > y !== yj > y &&
-      x < ((xj - xi) * (y - yi)) / (yj - yi) + xi;
+  const intersect =
+    ((yi > y) !== (yj > y)) &&
+    x < ((xj - xi) * (y - yi)) / (yj - yi) + xi;
 
     if (intersect) inside = !inside;
   }
