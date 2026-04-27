e44fc7e73f008579f570689b77cb38982481321e
Chris Catlin <109528250+ChrisStuff4Fun@users.noreply.github.com>
2026-04-27 20:35:32 +0100

    Bug fix: Syntax update
    Old documentation was being used, so code has been updated now

--- SourceCode/dotnet/Native/seal/cpp/seal_wrapper.cpp
+++ SourceCode/dotnet/Native/seal/cpp/seal_wrapper.cpp
@@ -34,31 +34,11 @@ void init_seal()
 
     // Generate keys
     secret_key = keygen.secret_key();
-    public_key = keygen.public_key();
-    relin_keys = keygen.relin_keys();
+    keygen.create_public_key(public_key);
+    keygen.create_relin_keys(relin_keys);
 
     encryptor = new Encryptor(*context, public_key);
     evaluator = new Evaluator(*context);
     decryptor = new Decryptor(*context, secret_key);
 }
 
-
-
-extern "C" __declspec(dllexport)
-void encrypt_location(double lat, double lon, double *out_lat, double *out_lon)
-{
-    double scale = pow(2.0, 40);
-
-    Plaintext pt_lat, pt_lon;
-
-    encoder->encode(lat, scale, pt_lat);
-    encoder->encode(lon, scale, pt_lon);
-
-    Ciphertext ct_lat, ct_lon;
-
-    encryptor->encrypt(pt_lat, ct_lat);
-    encryptor->encrypt(pt_lon, ct_lon);
-
-    *out_lat = lat;
-    *out_lon = lon;
-}
