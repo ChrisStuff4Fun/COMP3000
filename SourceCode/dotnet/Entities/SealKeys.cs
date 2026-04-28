6682156f06d815f0549ab8e9922307642b551a5d
Chris Catlin <109528250+ChrisStuff4Fun@users.noreply.github.com>
2026-04-28 23:58:53 +0100

    JSON serialisation to allow public as a var name

--- SourceCode/dotnet/Entities/SealKeys.cs
+++ SourceCode/dotnet/Entities/SealKeys.cs
@@ -1,6 +1,13 @@
+using System.Text.Json.Serialization;
+
 public class SealKeys
 {
+    [JsonPropertyName("public")]
     public string? Public { get; set; }
+
+    [JsonPropertyName("secret")]
     public string? Secret { get; set; }
+
+    [JsonPropertyName("relin")]
     public string? Relin { get; set; }
 }
