23e853fb632bfab3d046b505e7bdbaf3b9f3bc33
Chris Catlin <109528250+ChrisStuff4Fun@users.noreply.github.com>
2026-04-28 23:26:15 +0100

    Bug fix: Base64 encoder function

--- SourceCode/dotnet/Native/seal/cpp/sealKeygen.cpp
+++ SourceCode/dotnet/Native/seal/cpp/sealKeygen.cpp
@@ -1,10 +1,35 @@
 #include "seal/seal.h"
 #include <sstream>
+#include <string>
 
 using namespace seal;
 
 // import functions from wrapper
 extern "C" SEALContext* getContext();
+
+
+
+ // helper function to convert from binary to b64
+static std::string base64Encode(const std::string& input)
+{
+    static const char table[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+    std::string out;
+    int val = 0, valb = -6;
+    for (unsigned char c : input)
+    {
+        val = (val << 8) + c;
+        valb += 8;
+        while (valb >= 0)
+        {
+            out.push_back(table[(val >> valb) & 0x3F]);
+            valb -= 6;
+        }
+    }
+    if (valb > -6) out.push_back(table[((val << 8) >> (valb + 8)) & 0x3F]);
+    while (out.size() % 4) out.push_back('=');
+    return out;
+}
+
 
 extern "C" __declspec(dllexport)
 const char* generateKeys()
