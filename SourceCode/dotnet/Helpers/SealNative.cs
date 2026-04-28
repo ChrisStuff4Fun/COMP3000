f3c88a7d0e0b240aba0a86f1e1236c4bab2fbe8e
Chris Catlin <109528250+ChrisStuff4Fun@users.noreply.github.com>
2026-04-28 19:41:59 +0100

    Bug fix: Testing file rename

--- SourceCode/dotnet/Helpers/SealNative.cs
+++ SourceCode/dotnet/Helpers/SealNative.cs
@@ -2,9 +2,9 @@ using System.Runtime.InteropServices;
 
 public static class SealNative
 {
-    [DllImport("sealWrapper.dll", CallingConvention = CallingConvention.Cdecl)]
+    [DllImport("seal_wrapper.dll", CallingConvention = CallingConvention.Cdecl)]
     public static extern void initSeal();
 
-    [DllImport("sealKeygen.dll", CallingConvention = CallingConvention.Cdecl)]
+    [DllImport("seal_wrapper.dll", CallingConvention = CallingConvention.Cdecl)]
     public static extern IntPtr generateKeys();
 }
