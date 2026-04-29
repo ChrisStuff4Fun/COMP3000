e148c0b4b2a7acf8fc5dde87228b46b55bae2860
Chris Catlin <109528250+ChrisStuff4Fun@users.noreply.github.com>
2026-04-29 21:15:38 +0100

    Changing location var to string, since location will be encrypted

--- SourceCode/dotnet/Entities/Device.cs
+++ SourceCode/dotnet/Entities/Device.cs
@@ -5,8 +5,8 @@ public class Device
     [Key]
     public int DeviceID {get; set;}
     public string DeviceName {get; set;} = string.Empty;
-    public double LastLoggedLat {get; set;}
-    public double LastLoggedLong {get; set;}
+    public string LastLoggedLat {get; set;} = string.Empty;
+    public string LastLoggedLong {get; set;} = string.Empty;
     public int OrgID {get; set;}
     public string PublicKeyX {get; set;} = string.Empty;
     public string PublicKeyY {get; set;} = string.Empty;
