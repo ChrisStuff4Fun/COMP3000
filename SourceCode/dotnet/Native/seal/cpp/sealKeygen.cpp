3c0a3bc5d45b6d8bd68de15a6e6bd85c2931bd4f
Chris Catlin <109528250+ChrisStuff4Fun@users.noreply.github.com>
2026-04-30 22:03:33 +0100

    Added error handling

--- SourceCode/dotnet/Native/seal/cpp/sealKeygen.cpp
+++ SourceCode/dotnet/Native/seal/cpp/sealKeygen.cpp
@@ -12,35 +12,51 @@ extern "C" SEALContext* getContext();
 extern "C" __declspec(dllexport)
 const char* generateKeys()
 {
-    static std::string result;
 
-    SEALContext* ctx = getContext();
-    if (!ctx)
+    try {
+
+        static std::string result;
+
+        SEALContext* ctx = getContext();
+        if (!ctx)
+        {
+            result = "{\"public\":null,\"secret\":null,\"relin\":null,\"error\":\"context is null\"}";
+            return result.c_str(); // not initialised
+        }
+
+        KeyGenerator keygen(*ctx);
+
+        PublicKey pk;
+        SecretKey sk = keygen.secret_key();
+        RelinKeys rk;
+
+        keygen.create_public_key(pk);
+        keygen.create_relin_keys(rk);
+
+        std::ostringstream pk_stream, sk_stream, rk_stream;
+
+        pk.save(pk_stream);
+        sk.save(sk_stream);
+        rk.save(rk_stream);
+
+        result =
+            std::string("{\"public\":\"") + base64Encode(pk_stream.str()) +
+            "\",\"secret\":\"" + base64Encode(sk_stream.str()) +
+            "\",\"relin\":\"" + base64Encode(rk_stream.str()) +
+            "\"}";
+
+        return result.c_str();
+        
+    }
+    catch (const std::exception& e)
     {
-        result = "{\"public\":null,\"secret\":null,\"relin\":null,\"error\":\"context is null\"}";
-        return result.c_str(); // not initialised
+        result = std::string("{\"public\":null,\"error\":\"") + e.what() + "\"}";
+        return result.c_str();
+    }
+    catch (...)
+    {
+        result = "{\"public\":null,\"error\":\"unknown exception in generateKeys\"}";
+        return result.c_str();
     }
 
-    KeyGenerator keygen(*ctx);
-
-    PublicKey pk;
-    SecretKey sk = keygen.secret_key();
-    RelinKeys rk;
-
-    keygen.create_public_key(pk);
-    keygen.create_relin_keys(rk);
-
-    std::ostringstream pk_stream, sk_stream, rk_stream;
-
-    pk.save(pk_stream);
-    sk.save(sk_stream);
-    rk.save(rk_stream);
-
-    result =
-        std::string("{\"public\":\"") + base64Encode(pk_stream.str()) +
-        "\",\"secret\":\"" + base64Encode(sk_stream.str()) +
-        "\",\"relin\":\"" + base64Encode(rk_stream.str()) +
-        "\"}";
-
-    return result.c_str();
 }
